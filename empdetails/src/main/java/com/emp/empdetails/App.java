88761db29579fc2360be8d94e9e04bf042e91f3a
sreedevi27 <sree_devi27@yahoo.com>
2020-02-04 14:08:47 -0600

    empdetails commited

---
+++ empdetails/src/main/java/com/emp/empdetails/App.java
@@ -0,0 +1,13 @@
+package com.emp.empdetails;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
